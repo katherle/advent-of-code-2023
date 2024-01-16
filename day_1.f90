759e15f48425e206183fadeafe92c536a15cad66
Katherine Lee <klee@mpia.de>
2024-01-16 15:04:50 +0100

    finished day 1, got right answer

--- day_1.f90
+++ day_1.f90
@@ -8,26 +8,32 @@ PROGRAM day_1
     ! find the sum of all these numbers
     ! WRITE (*,*) 'Result = ', sum
 
-    INTEGER :: status, 
-    CHARACTER(*) :: ind_a, ind_b, a, b 
-    INTEGER :: c
-    INTEGER :: sum = 0
+    INTEGER i ! line in calibration file
+    INTEGER :: ind_a ! index of first integer in calib
+    INTEGER :: ind_b ! index of last integer in calib
+    CHARACTER(2) :: c ! concatenation 
+    INTEGER :: int_c ! integer version of c
+    INTEGER :: s = 0 ! sum over all c
+    INTEGER :: status
     CHARACTER(80) :: msg
-    CHARACTER(len = *) :: calib
+    CHARACTER(100) :: calib
 
     OPEN (UNIT = 1, FILE = "input/day_1.txt", STATUS = "OLD", ACTION = "READ", IOSTAT = status, IOMSG = msg)
         IF (status == 0) THEN
             DO i = 1, 1000
                 READ (1, *, IOSTAT = status) calib ! get next line in text file
                 IF (status /= 0) EXIT ! if not valid then exit
+
                 ind_a = SCAN(calib, "0123456789") ! scan through calib finding the first digit
                 ind_b = SCAN(calib, "0123456789", .TRUE.) ! scan through calib finding the last digit
 
-                a = calib(iachar(ind_a))
-                b = calib(iachar(ind_b))
-                c = iachar(a//b) ! concatenate a and b and convert to type integer
-                sum = sum + c
+                c = calib(ind_a:ind_a) // calib(ind_b:ind_b)
+                read (c, '(I2)') int_c
+
+                s = s + int_c
             END DO
+            WRITE(*,*) "result: ", s
+        END IF
     CLOSE(UNIT = 1)
-    WRITE(*, *) 'Result = '‚ c
+
 END PROGRAM day_1
