c35884f2f32afece79bf4a8e1447f59519e2bb30
Katherine Lee <klee@mpia.de>
2024-01-19 13:02:14 +0100

    solved day 1 part 2

--- day_1.f90
+++ day_1.f90
@@ -9,29 +9,62 @@ PROGRAM day_1
     ! WRITE (*,*) 'Result = ', sum
 
     INTEGER i ! line in calibration file
+    INTEGER j ! element of nums
+    CHARACTER(100) :: calib
     INTEGER :: ind_a ! index of first integer in calib
     INTEGER :: ind_b ! index of last integer in calib
-    CHARACTER(2) :: c ! concatenation 
+    CHARACTER(5), DIMENSION(9) :: nums
+    CHARACTER(1), DIMENSION(9) :: int_nums
+    INTEGER :: ind_nums_a ! index of element in nums, searching from front
+    INTEGER :: ind_nums_b ! index of element in nums, searching from back
+    CHARACTER(1) :: calib_a ! value of first integer in calib
+    CHARACTER(1) :: calib_b ! value of last integer in calib
+    CHARACTER(2) :: c ! concatenation of calib_a and calib_b
     INTEGER :: int_c ! integer version of c
     INTEGER :: s = 0 ! sum over all c
     INTEGER :: status
     CHARACTER(80) :: msg
-    CHARACTER(100) :: calib
+
+    nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    int_nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     OPEN (UNIT = 1, FILE = "input/day_1.txt", STATUS = "OLD", ACTION = "READ", IOSTAT = status, IOMSG = msg)
         IF (status == 0) THEN
-            DO i = 1, 1000
+            file_iter: DO i = 1, 1000
                 READ (1, *, IOSTAT = status) calib ! get next line in text file
                 IF (status /= 0) EXIT ! if not valid then exit
 
                 ind_a = SCAN(calib, "0123456789") ! scan through calib finding the first digit
-                ind_b = SCAN(calib, "0123456789", .TRUE.) ! scan through calib finding the last digit
+                ind_b = SCAN(calib, "0123456789", BACK = .TRUE.) ! scan through calib finding the last digit
+            
+                calib_a = calib(ind_a:ind_a)
+                calib_b = calib(ind_b:ind_b)
+            
+                number_iter: DO j = 1, 9
+                    ind_nums_a = INDEX(calib, TRIM(nums(j)))
+                    ind_nums_b = INDEX(calib, TRIM(nums(j)), .TRUE.)
+                    IF (ind_a == 0 .AND. ind_nums_a /= 0) THEN
+                        ind_a = ind_nums_a
+                        ind_b = ind_nums_b
+                        calib_a = int_nums(j)
+                        calib_b = int_nums(j)
+                    END IF
+                    IF (ind_nums_a < ind_a .AND. ind_nums_a /= 0) THEN
+                        ind_a = ind_nums_a
+                        calib_a = int_nums(j)
+                    END IF
+                    IF (ind_nums_b > ind_b .AND. ind_nums_b /= 0) THEN 
+                        ind_b = ind_nums_b
+                        calib_b = int_nums(j)
+                    END IF
+                END DO number_iter
 
-                c = calib(ind_a:ind_a) // calib(ind_b:ind_b)
+                c = calib_a // calib_b
+            
                 read (c, '(I2)') int_c
 
                 s = s + int_c
-            END DO
+            END DO file_iter
             WRITE(*,*) "result: ", s
         END IF
     CLOSE(UNIT = 1)
