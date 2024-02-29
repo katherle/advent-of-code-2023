d4f4cba3349036da0930b37d2efa202597ba1d75
Katherine Lee <klee@mpia.de>
2024-02-29 14:49:34 +0100

    finished day 3 part 1 (finally)

--- day_3_part1.f90
+++ day_3_part1.f90
@@ -14,46 +14,35 @@ PROGRAM day_3
     integer :: num ! sum of all numbers on each line adjacent to symbols
     integer :: sum = 0 ! sum of all numbers in input file adjacent to symbols
 
-    character(12) :: input_1 !read in 3 lines at a time
-    character(12) :: input_2 
-    character(12) :: input_3 
+    character(140) :: input_1 !read in 3 lines at a time
+    character(140) :: input_2 
+    character(140) :: input_3 
 
-    OPEN(UNIT = 1, FILE = "input/day_3_test2.txt", STATUS = "OLD", ACTION = "READ", IOSTAT = status, IOMSG = msg)
+    OPEN(UNIT = 1, FILE = "input/day_3.txt", STATUS = "OLD", ACTION = "READ", IOSTAT = status, IOMSG = msg)
         status_check: IF (status == 0) THEN
             ! read in line of interest and the 2 surrounding
             ! start with a line containing no symbols so subroutine can check line 1
-            !input_1 = "............................................................................................................................................"
-            input_1 = "............"
-            read(1, '(A12)', IOSTAT = status) input_2
-            read(1, '(A12)', IOSTAT = status) input_3
-            !print *,input_1
-            !print *,input_2
-            !print *,input_3
+            input_1 = "............................................................................................................................................"
+            read(1, '(A140)', IOSTAT = status) input_2
+            read(1, '(A140)', IOSTAT = status) input_3
 
-            DO line = 1, 12
+            file_iter: DO line = 1, 140
                 num = 0
-                !print *,input_1
-                !print *,input_2
-                !print *,input_3
                 print *,"line: ",line
                 CALL num_if_symbol(input_1, input_2, input_3, num) ! subroutine sums all numbers on the line that are adjacent to symbols
                 print *,"Num on this line: ", num
             
-                sum = sum + num ! add result of subroutine to existing sum
+                sum = sum + num ! add result to existing sum
 
                 input_1 = input_2
                 input_2 = input_3
-                IF (line == 11) THEN
+                IF (line == 140) THEN
                     ! insert line at end containing no symbols so subroutine can check the last line of the input
-                    !input_3 = "............................................................................................................................................"
-                    input_3 = "............"
+                    input_3 = "............................................................................................................................................"
                 ELSE
-                    read(1, '(A12)', IOSTAT = status) input_3
+                    read(1, '(A140)', IOSTAT = status) input_3
                 END IF
-            END DO
-
-            !CALL num_if_symbol(input_1, input_2, input_3, num)
-            !sum = sum + num
+            END DO file_iter
 
             print *,"Sum = ", sum
 
@@ -81,91 +70,73 @@ SUBROUTINE num_if_symbol(input_1, input_2, input_3, num)
     integer :: index ! iterable, goes inside a do statement
 
     ind_a = scan(input_2, "0123456789")
-    IF (ind_a == 0) THEN
-        print *,"no numbers on this line"
+    IF (ind_a == 0) THEN 
+        ! no numbers in the line
         RETURN
-    ELSE
-        print *,"numbers exist"
     END IF
 
-    ind_b = scan(input_2(ind_a:ind_a+3), ".") + ind_a - 2 
-    IF (ind_b < ind_a) THEN
+    ind_b = scan(input_2(ind_a:ind_a+2), "0123456789", .true.) + ind_a - 1 
+    IF (ind_b < ind_a) THEN 
+        ! this number is at the end of the line
         ind_b = 0
         temp_cha = input_2(ind_a:)
     ELSE
         temp_cha = input_2(ind_a:ind_b)
     END IF
-    !print *,temp_cha
 
-    DO
-        !print *,input_1
-        !print *,input_2
-        !print *,input_3
-        !print *,ind_a, ind_b
-
-        IF (ind_a == 1) THEN
-            print *,"number at beginning of line"
+    line_iter: DO
+        symbol_check: IF (ind_a == 1) THEN
+            ! number at beginning of line
             DO index = 2, len(trim(temp_cha)) + 2
-                symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
-                symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
+                symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789") ! search for symbols in line above
+                symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789") ! symboles in line below
             END DO
-            symbol_2(2) = verify(input_2(ind_b+1:ind_b+1), ".")
+            symbol_2(2) = verify(input_2(ind_b+1:ind_b+1), ".") ! symbols in the same line
         ELSE IF (ind_b == 0) THEN
-            print *,"number at end of line"
-            print *,ind_a,ind_b
-            DO index = 1, len(trim(temp_cha)) + 2
+            ! number at end of line
+            DO index = 1, len(trim(temp_cha)) + 1
                 symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
                 symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
-                !print *,input_3(ind_a+index-2:ind_a+index-2)
             END DO
             symbol_2(1) = verify(input_2(ind_a-1:ind_a-1), ".")
         ELSE
-            print *,"number within line"
-            !print *,len(trim(temp_cha))
-            !print *,ind_a,ind_b
+            ! number within line
             DO index = 1, len(trim(temp_cha))+2
                 symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
                 symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
             END DO
             symbol_2(1) = verify(input_2(ind_a-1:ind_a-1), ".")
             symbol_2(2) = verify(input_2(ind_b+1:ind_b+1), ".")
-            !print *,input_2(ind_b:ind_b)
-        END IF
+        END IF symbol_check
     
-        IF (any(symbol_1) .or. any(symbol_2) .or. any(symbol_3)) THEN
-            print *,"Number adjacent to a symbol!"
-            print *,temp_cha
-            read (temp_cha,*) temp
+        adjacent: IF (any(symbol_1) .or. any(symbol_2) .or. any(symbol_3)) THEN
+            ! number is adjacent to a symbol
+            read (temp_cha,*) temp !convert character variable to integer
             num = num + temp
-            !print *,ind_a,ind_b
-        ELSE
-            print *,"Number not adjacent to a symbol."
-            print *,temp_cha
-            num = num
-        END IF
-        !print *,num
+        END IF adjacent
     
-        ind_a = scan(input_2(ind_b+1:), "0123456789") + ind_b
-        IF (ind_a == ind_b .or. ind_b == 0) THEN
-            print *,"Exiting"
+        ind_a = scan(input_2(ind_b+1:), "0123456789") + ind_b ! search for more numbers in the line
+        get_next: IF (ind_a == ind_b .or. ind_b == 0) THEN
+            ! no more numbers
+            ! reset boolean array 
             symbol_1 = [0,0,0,0,0]
             symbol_2 = [0,0]
             symbol_3 = [0,0,0,0,0]
             RETURN
         ELSE
-            ind_b = scan(input_2(ind_a:ind_a+2), ".") + ind_a - 2
-            IF (ind_b < ind_a) THEN
+            ind_b = scan(input_2(ind_a:ind_a+2), "0123456789", .true.) + ind_a - 1 ! find the end of the next number
+            IF (ind_b == 140) THEN
+                ! this number is the last one in the line
                 ind_b = 0
                 temp_cha = input_2(ind_a:)
             ELSE
                 temp_cha = input_2(ind_a:ind_b)
             END IF
+            ! reset boolean array
             symbol_1 = [0,0,0,0,0]
             symbol_2 = [0,0]
             symbol_3 = [0,0,0,0,0]
-            temp_cha = trim(input_2(ind_a:ind_b))
-            !temp_cha = trim(temp_cha)
-        END IF
-    END DO
+        END IF get_next 
+    END DO line_iter
 
 END SUBROUTINE num_if_symbol
--- test.f90
+++ test.f90
@@ -1,29 +1,83 @@
 PROGRAM test
     IMPLICIT NONE
 
-    character(10) :: input_1 = ".........."
-    character(10) :: input_2 = "467..114.." 
-    character(10) :: input_3 = "...*......"
+    character(140) :: input_1 = "......81$.....544..67...............*.....159....*............209.747*29........./..........812.........430.232...................199*587..."
+    character(140) :: input_2 = "760.................*...#........331.................%...158...................#.....................29.................596...477..........." 
+    character(140) :: input_3 = "..........%./....922...388..970...............510...68......*....590....545...621...-..977.593..889...*.........................=..879..*389"
 
-    integer ind_a, ind_b
+    integer :: ind_a, ind_b, index, temp
+    character(3) :: temp_cha
+    integer ::  num = 0
+
+    logical, dimension(5) :: symbol_1, symbol_3 = [0,0,0,0,0]
+    logical, dimension(2) :: symbol_2 = [0,0]
 
     ind_a = scan(input_2, "0123456789")
-    ind_b = scan(input_2(ind_a:ind_a+3), "0123456789", .true.)
+    ind_b = scan(input_2(ind_a:ind_a+2), "0123456789", .true.) + ind_a - 1
     print *,ind_a
     print *,ind_b
 
-    print *,input_2
+    IF (ind_b < ind_a) THEN
+        ind_b = 0
+        temp_cha = input_2(ind_a:)
+    ELSE
+        temp_cha = input_2(ind_a:ind_b)
+    END IF
 
-    DO
+    print *,temp_cha
+
+    DO 
+        IF (ind_a==1) THEN
+            print *,"Number at beginning of line"
+            DO index = 2, len(trim(temp_cha)) + 2
+                symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
+                symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
+            END DO
+            symbol_2(2) = verify(input_2(ind_b+1:ind_b+1), ".")
+        ELSE IF (ind_b==0) THEN
+            print *,"Number at end of line"
+            DO index = 1, len(trim(temp_cha)) + 1
+                symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
+                symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
+            END DO
+            symbol_2(1) = verify(input_2(ind_a-1:ind_a-1), ".")
+        ELSE
+            print *,"Number within line"
+            DO index = 1, len(trim(temp_cha)) + 2
+                symbol_1(index) = verify(input_1(ind_a+index-2:ind_a+index-2), ".0123456789")
+                symbol_3(index) = verify(input_3(ind_a+index-2:ind_a+index-2), ".0123456789")
+            END DO
+            symbol_2(1) = verify(input_2(ind_a-1:ind_a-1), ".")
+            symbol_2(2) = verify(input_2(ind_b+1:ind_b+1), ".")
+        END IF
+
+        IF (any(symbol_1) .or. any(symbol_2) .or. any(symbol_3)) THEN
+            print *,"Number adjacent to a symbol!"
+            read (temp_cha,*) temp
+            num = num + temp
+        ELSE
+            print *,"Number not adjacent to a symbol."
+        END IF
+
         ind_a = scan(input_2(ind_b+1:), "0123456789") + ind_b
-        IF (ind_a == ind_b) THEN
-            print *,"No other numbers in the line"
+        IF (ind_a == ind_b .or. ind_b == 0) THEN
+            print *,"Exiting"
             EXIT
         ELSE
-            ind_b = scan(input_2(ind_a:ind_a+3), "0123456789", .true.) + ind_a
-            print *,ind_a
-            print *,ind_b
+            ind_b = scan(input_2(ind_a:ind_a+2), "0123456789", .true.) + ind_a - 1
+            IF (ind_b == 140) THEN
+                ind_b = 0
+                temp_cha = input_2(ind_a:)
+            ELSE
+                temp_cha = input_2(ind_a:ind_b)
+            END IF
+            print *,temp_cha
+            symbol_1 = [0,0,0,0,0]
+            symbol_2 = [0,0]
+            symbol_3 = [0,0,0,0,0]
         END IF
     END DO
+
+    print *,"Num on this line: ",num
 
 END PROGRAM test
